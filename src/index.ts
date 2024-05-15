bce6a52375d7c3850e2aa55e5fc3dfa8bb717a91
Yugang Cao <yugang.cao12@gmail.com>
2024-05-15 13:31:50 +0800

    chore: quick fold/unfold

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import { window } from 'vscode'
+import * as vscode from 'vscode';
 
-export function activate() {
-  window.showInformationMessage('Hello')
+export function activate(context: vscode.ExtensionContext) {
+  let disposableFold = vscode.commands.registerCommand('extension.foldAll', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      vscode.commands.executeCommand('editor.foldAll');
+    }
+  });
+
+  let disposableUnfold = vscode.commands.registerCommand('extension.unfoldAll', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      vscode.commands.executeCommand('editor.unfoldAll');
+    }
+  });
+
+  context.subscriptions.push(disposableFold);
+  context.subscriptions.push(disposableUnfold);
 }
 
-export function deactivate() {
-
-}
+export function deactivate() {}

053be326d7146d78761e73f9f56aeefbdbbb160b
Yugang Cao <yugang.cao12@gmail.com>
2024-05-15 20:35:44 +0800

    fix: lint

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
+/**
+ *
+ * @param context
+ */
 export function activate(context: vscode.ExtensionContext) {
-  let disposableFold = vscode.commands.registerCommand('extension.foldAll', () => {
-    const editor = vscode.window.activeTextEditor;
-    if (editor) {
-      vscode.commands.executeCommand('editor.foldAll');
-    }
-  });
+  const disposableFold = vscode.commands.registerCommand('extension.foldAll', () => {
+    const editor = vscode.window.activeTextEditor
+    if (editor)
+      vscode.commands.executeCommand('editor.foldAll')
+  })
 
-  let disposableUnfold = vscode.commands.registerCommand('extension.unfoldAll', () => {
-    const editor = vscode.window.activeTextEditor;
-    if (editor) {
-      vscode.commands.executeCommand('editor.unfoldAll');
-    }
-  });
+  const disposableUnfold = vscode.commands.registerCommand('extension.unfoldAll', () => {
+    const editor = vscode.window.activeTextEditor
+    if (editor)
+      vscode.commands.executeCommand('editor.unfoldAll')
+  })
 
-  context.subscriptions.push(disposableFold);
-  context.subscriptions.push(disposableUnfold);
+  context.subscriptions.push(disposableFold)
+  context.subscriptions.push(disposableUnfold)
 }
 
+/**
+ *
+ */
 export function deactivate() {}
